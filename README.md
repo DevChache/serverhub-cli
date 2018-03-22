2a72128bce9082802a4441c0a880836753d15d89
Yang Zhongdong <yangzd1996@outlook.com>
2018-03-22 13:15:07 +0800

    Feature: Add template loading and replace old style arguments

--- README.md
+++ README.md
@@ -4,11 +4,35 @@ This is the CLI tool for [serverhub-mvc](https://www.npmjs.com/package/serverhub
 
 ## About
 
-This tool is only for generating basic file structure for serverhub-mvc, and you can get a demo with option `-d`.
+This tool is only for generating file structure or templates for serverhub-mvc.
 
 Any detailed information about usage, please refer to `serverhub-cli -h`.
+
+## Usage
+
+Here is an example using serverhub-cli to intialize an template with built-in shortcut 'template'.
+
+First, you need to create a directory and set it as working directory.
+
+```bash
+mkdir demo && cd demo
+```
+
+Then use serverhub-cli to set up the template.
+
+```bash
+serverhub-cli init template
+```
+
+Bingo! The target template is successfully loaded.
+
+## Templates
+
+Since version 0.1.0, ServerHub updated the way of generating templates. We will list several avaliable templates in later updates.
 
 ---
 CHINESE version:
 
 这是 ServerHub MVC 项目的命令行工具，用来生成所需的目录结构，还可以生成一个 demo。如果你需要任何的使用帮助，可以在安装后使用 `serverhub-cli -h` 命令来获取说明。
+
+ServerHub CLI 在 0.1.0 版本之后就不再使用 `-d` 选项来修饰 `init` 参数了。但是我们提供了更多元的模板支持，详细的内容会逐步更新。
--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const program = require('commander');
 const package = require('./package.json');
 const path = require('path');
 const fs = require('fs-extra');
+const spawnSync = require('child_process').spawnSync;
+const spawn = require('child_process').spawn;
 
 function createWorkspace(targetpath, demo) {
     let app_js = fs.readFileSync(path.resolve(__dirname, './content/app.js'));
@@ -29,44 +31,145 @@ function createWorkspace(targetpath, demo) {
     console.log('Don\'t forget to install and import serverhub module before run `node app.js`');
 }
 
+const gitRegex = /^(?:https:\/\/)?github\.com\/[a-zA-Z\d]*(?:[a-zA-Z\d]+-)*[a-zA-Z\d]+\/[a-zA-Z\d]*(?:[a-zA-Z\d]+-)*[a-zA-Z\d]+\.git$/g;
+
+const templates = [{
+    name: 'template',
+    git: 'https://github.com/ServerHubOrg/serverhub-template.git'
+}];
+
+const gitstructures = ['.git/', '.gitignore'];
+function removeGitStructure(targetpath) {
+    if (targetpath === '/') {
+        console.error('Illegal operation!');
+        process.exit(1);
+    }
+    let noerr = true;
+    gitstructures.forEach(t => {
+        let ta = path.resolve(targetpath, t);
+        let clean_process = spawn('rm', ['-rf', ta]);
+        clean_process.on('exit', (code, signal) => {
+            if (code !== 0) {
+                console.error(`Cannot clean target ${ta}`);
+                noerr = false;
+            } else {
+                console.log(`Cleaned target ${ta}`);
+            }
+        });
+        clean_process.stdout.on('data', (m) => {
+            console.log(m.toString());
+        });
+        clean_process.stderr.on('data', (m) => {
+            console.error(m.toString());
+        });
+    });
+}
+
+function cloneGitRepo(url) {
+    console.log(`Ready to clone template from ${url}`);
+    let git_clone_process = spawn('git', ['clone', url, './']);
+    git_clone_process.on('exit', (code, signal) => {
+        if (code !== 0) {
+            console.error('Cannot perform clone operations.');
+            process.exit(1);
+        } else {
+            console.log('Cloned with no error.');
+            removeGitStructure(process.cwd());
+        }
+    });
+    git_clone_process.on('message', (m) => {
+        console.log(m.toString());
+    })
+    git_clone_process.stdout.on('data', (m) => {
+        console.log(m.toString());
+    });
+    git_clone_process.stderr.on('data', (m) => {
+        console.error(m.toString());
+    });
+}
+
+function processTemplate(target) {
+    if (gitRegex.test(target)) {
+        // git address.
+        console.error(`${package.version} of serverhub-cli does not support 3-party templates.`);
+        process.exit(1);
+    } else {
+        // shortcut.
+        let found = false;
+        templates.forEach(template => {
+            if (template.name === target) {
+                // found template;
+                cloneGitRepo(template.git);
+                found = true;
+            }
+        });
+        if (!found) {
+            console.error('No such template!');
+            process.exit(1);
+        }
+    }
+}
+
 program
     .version(package.version)
     .usage('serverhub [command] [options]')
 
 program
-    .command('init [directory]')
-    .option('-h, --hard', "Force initialize the workspace")
-    .option('-d, --demo', "Create a demo in the directory")
-    .action((directory, options) => {
-        if (!directory) {
-            console.log("Target directory is required.");
+    .command('init [target]')
+    .option('-f, --force', "Force initialize the workspace")
+    .action((target, options) => {
+        if (!target) {
+            console.log("Initialization configuration type/name is required.");
             process.exit(1);
         }
-        let targetPath = path.resolve(process.cwd(), directory);
-        console.log('About to create workspace in: ', targetPath, '\n');
-        if (options.hard) {
-            fs.emptyDirSync(targetPath);
-            console.log('Directory cleared');
-            createWorkspace(targetPath, options.demo ? true : false);
-        } else {
-            fs.readdir(targetPath, (err, files) => {
-                if (!err) {
-                    if (!options.hard && files.length > 0) {
-                        console.log('Directory not empty, try clean it manually or add -h option');
-                    } else {
-                        createWorkspace(targetPath, options.demo ? true : false);
+
+        if (target === 'bare') {
+            let targetPath = path.resolve(process.cwd());
+            console.info('About to create workspace in: ', targetPath, '\n');
+            if (options.hard) {
+                fs.emptyDirSync(targetPath);
+                console.info('Directory cleared');
+                createWorkspace(targetPath, true);
+            } else {
+                fs.readdir(targetPath, (err, files) => {
+                    if (!err) {
+                        if (!options.hard && files.length > 0) {
+                            console.log('Directory is not empty, try clean it manually or add -f option');
+                        } else {
+                            createWorkspace(targetPath, options.demo ? true : false);
+                        }
                     }
-                }
-            });
+                });
+            }
+        } else {
+            if (fs.readdirSync(process.cwd()).length !== 0 && !options.hard) {
+                console.error('Directory is not empty. Try -f/--force to ignore the warning.');
+                process.exit(1);
+            } else {
+                if (fs.readdirSync(process.cwd()).length !== 0 && options.hard)
+                    fs.emptyDirSync(process.cwd());
+                processTemplate(target);
+            }
         }
     });
 
 program.command('help <cmd>')
     .action((cmd) => {
         if (cmd === 'init') {
-            console.log(`Create workspace for serverhub to run.\n    use -h, --hard to create workspace even when directory not empty.\n    use -d, --demo to add demo files.`);
+            console.log(`Create workspace for serverhub to run.
+            
+ARGS:
+- target: The target type of the template you want to initialize.
+  supported target:
+  > bare: create an empty template with basic configuration and support files.
+  > [git-repository]: example: https://github.com/ServerHubOrg/serverhub-template.git.
+  
+OPTIONS:
+-f, --force: To create workspace even when directory not empty.`);
         } else if (cmd === 'help') {
             console.log(`Print the help message of any command.\n use serverhub-cli help <command>.`);
+        } else {
+            console.error(`${cmd} is not a valid argument.`);
         }
     })
 program.parse(process.argv);
